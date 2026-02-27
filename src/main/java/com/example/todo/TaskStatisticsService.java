9b66d46ce140f69faa10b3ef327211b5a277aa1d
Safronov Maxim Pavlovich <163042437+MaksiSafronov@users.noreply.github.com>
2026-02-27 18:58:16 +0300

    Made TaskStatisticsService

---
+++ src/main/java/com/example/todo/TaskStatisticsService.java
@@ -0,0 +1,36 @@
+package com.example.todo;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskStatisticsService {
+
+    private final TaskRepository primaryRepository;
+    private final TaskRepository stubRepository;
+
+    public TaskStatisticsService(TaskRepository taskRepository,
+                                 @Qualifier("stubTaskRepository") TaskRepository stubRepository) {
+        this.primaryRepository = taskRepository;
+        this.stubRepository = stubRepository;
+    }
+
+    public int getPrimaryRepositoryTaskCount() {
+        return primaryRepository.findAll().size();
+    }
+
+    public int getStubRepositoryTaskCount() {
+        return stubRepository.findAll().size();
+    }
+
+    public List<Task> getPrimaryTasks() {
+        return primaryRepository.findAll();
+    }
+
+    public List<Task> getStubTasks() {
+        return stubRepository.findAll();
+    }
+}
+
