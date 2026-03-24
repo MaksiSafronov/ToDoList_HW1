dcdd5d89ec843071879c0b7171f99333511dc28b
Safronov Maxim Pavlovich <163042437+MaksiSafronov@users.noreply.github.com>
2026-03-24 15:27:34 +0300

    made dto classes

---
+++ src/main/java/com/example/todo/dto/TaskUpdateDto.java
@@ -0,0 +1,64 @@
+package com.example.todo.dto;
+
+import com.example.todo.model.Priority;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+public class TaskUpdateDto {
+
+    private String title;
+    private String description;
+    private boolean completed;
+    private LocalDate dueDate;
+    private Priority priority;
+    private Set<String> tags;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public Set<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(Set<String> tags) {
+        this.tags = tags;
+    }
+}
